5ccffefd58e4ca085204fc784bebba165fed49f1
Jefferson <jefferson.leal@7comm.com.br>
2023-01-10 15:38:30 -0300

    configurando as propriedades de conexão do gmail

--- src/test/java/br/com/email/mania/AppTest.java
+++ src/test/java/br/com/email/mania/AppTest.java
@@ -1,5 +1,7 @@
 package br.com.email.mania;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,6 +10,13 @@ public class AppTest {
 	public void testeEmail() {
 		
 		/*Olhar as configurações do SMTP do seu email de origem*/
+		Properties properties = new Properties();
+		properties.put("mail.smtp.aith", "true");//Autorização
+		properties.put("mail.smtp.starttls", "true");//Autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com");// Servidor do gmail
+		properties.put("mail.smtp.port", "456");// Porta padrão do servidor
+		properties.put("main.smtp.socketFactory.prot", "465");//Especifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//Classe socket de conexão ao SMTP
 	}
 
 }
