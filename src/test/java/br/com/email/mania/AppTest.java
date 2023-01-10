22a6a97e3b4db7ceae74681b661f37144a7a05c3
Jefferson <jefferson.leal@7comm.com.br>
2023-01-10 15:18:24 -0300

    Criada estrutura inicial do nosso projeto

---
+++ src/test/java/br/com/email/mania/AppTest.java
@@ -0,0 +1,13 @@
+package br.com.email.mania;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testeEmail() {
+		
+		/*Olhar as configurações do SMTP do seu email de origem*/
+	}
+
+}
