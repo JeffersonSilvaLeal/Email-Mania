101e5a0197bf54702296903253b61287fa5157e1
Jefferson <jefferson.leal@7comm.com.br>
2023-01-11 14:19:33 -0300

    Enviando e-mail com PDF em anexo

--- src/main/java/br/com/email/mania/model/ObjetoEnviaEmail.java
+++ src/main/java/br/com/email/mania/model/ObjetoEnviaEmail.java
@@ -3,16 +3,24 @@ package br.com.email.mania.model;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
 import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
@@ -24,7 +32,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class ObjetoEnviaEmail {
 
-	private String userName = "seuEmail";
+	private String userName = "seuEmail@gmail.com";
 	private String password = "suaSenha";
 	private String listaDestinatarios = "";
 	private String nomeRemetente = "";
@@ -79,6 +87,60 @@ public class ObjetoEnviaEmail {
 		}
 	}
 
+	public void enviarEmailAnexo(boolean envioHtml) {
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.ssl.trust", "*");
+			properties.put("mail.smtp.auth", "true");// Autorização
+			properties.put("mail.smtp.starttls", "true");// Autenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com");// Servidor do gmail
+			properties.put("mail.smtp.port", "465");// Porta padrão do servidor
+			properties.put("mail.smtp.socketFactory.port", "465");// Especifica a porta a ser conectada pelo socket
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");// Classe socket de
+																								// conexão ao
+																								// SMTP
+
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+
+			Address[] toUser = InternetAddress.parse(listaDestinatarios);
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName, nomeRemetente));// Quem está enviando
+			message.setRecipients(Message.RecipientType.TO, toUser);// Email de destino
+			message.setSubject(assuntoEmail);// Assunto do email
+			
+			// descrição do e-mail
+			MimeBodyPart corpoEmail = new MimeBodyPart();
+			
+			if (envioHtml) {
+				corpoEmail.setContent(mensagemEmail, "text/html; charset=utf-8");
+			} else {
+				corpoEmail.setText(mensagemEmail);
+			}
+
+			// anexo em pdf
+			MimeBodyPart anexoEmail = new MimeBodyPart();
+			anexoEmail.setDataHandler(new DataHandler(new ByteArrayDataSource(simuladorDePDF(), "apllication/pdf")));
+			anexoEmail.setFileName("anexoemail.pdf");
+			
+			//Junta as duas partes do email
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(corpoEmail);
+			multipart.addBodyPart(anexoEmail);
+			
+			message.setContent(multipart);
+			
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * 
 	 * Esse método simula um PDF ou qualquer arquivo enviado em anexo por email
@@ -90,7 +152,7 @@ public class ObjetoEnviaEmail {
 		file.createNewFile();
 		PdfWriter.getInstance(document, new FileOutputStream(file));
 		document.open();
-		document.add(new Paragraph("Conteudo em anexo em JavaMail"));
+		document.add(new Paragraph("Olá este é apenas um paragráfo teste se voçê está lendo ele é porque funcionou!!!"));
 		document.close();
 		return new FileInputStream(file);
 
--- src/test/java/br/com/email/mania/AppTest.java
+++ src/test/java/br/com/email/mania/AppTest.java
@@ -20,4 +20,19 @@ public class AppTest {
 		
 		objetoEnviaEmail.enviarEmail(true);
 	}
+	
+	@Test
+	public void testeEmailAnexo() {
+		
+		StringBuilder builderTextoEmail = new StringBuilder();
+		builderTextoEmail.append("Olá, tudo bem?<br/><br/>");
+		builderTextoEmail.append("Voçê está recebendo esse email de javaMail, gosta de <b>StarWars</b>?<br/><br/>");
+		builderTextoEmail.append("Click no Link abaixo,<br/><br/>");
+		builderTextoEmail.append("<a target=\"_blank\" href=\"https://www.starwars.com\" style=\"color:#39ff14; padding: 14px 25px; text-align:center; text-decoration:none; display:inline-block; border-radius:30px; font-size:20px; font-family:courier; border:3px solid green; background-color:#000000;\">StarWars</a><br/><br/>");
+		builderTextoEmail.append("<span style=\"font-size:10px; text-align:center;\">By: Jefferson da Silva Leal</span>");
+
+		ObjetoEnviaEmail objetoEnviaEmail = new ObjetoEnviaEmail("drankhar@gmail.com", "Jefferson Da Silva Leal", "Testando Email com JAVA", builderTextoEmail.toString());
+		
+		objetoEnviaEmail.enviarEmailAnexo(true);
+	}
 }
